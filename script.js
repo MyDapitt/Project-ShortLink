e37db3ecbca4296660bedd7429638170a57b0c51
SazumiVicky <vicihidayat722@gmail.com>
2023-06-18 13:56:19 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('form');
+const shortenResult = document.querySelector('#shorten-result');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const originalUrl = document.querySelector('#original-url').value;
+
+  const response = await fetch('/api/shorten', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ originalUrl })
+  });
+
+  const { shortUrl } = await response.json();
+
+  shortenResult.innerHTML = `Shorten Result: <a href="${shortUrl}">${shortUrl}</a>`;
+});
